08d8fd335bb80bcac02b08e56b9da87c415105d6
seyi olumuyiwa <dagaria964@gmail.com>
2025-09-07 22:02:28 +0100

    3rd fix for task 4

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav
       style={{
-        background: "#333",
+        backgroundColor: "#333",
         padding: "10px",
         display: "flex",
         justifyContent: "center",
