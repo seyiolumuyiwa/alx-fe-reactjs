4fbe89dcaf6fa2053480c4d91957c2cbdd4a78d0
seyi olumuyiwa <dagaria964@gmail.com>
2025-09-07 13:32:17 +0100

    Project two

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Footer = () => {
+  return (
+<footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center' }}>
+  <p>© 2023 City Lovers</p>
+</footer>
+  );
+};
+
+export default Footer;
