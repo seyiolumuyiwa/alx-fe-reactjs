23eb9ed5b637e5b76bdf5dff18f407f58f5b68b7
seyi olumuyiwa <dagaria964@gmail.com>
2025-10-18 02:31:06 +0100

    1st fix for task 3

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./components/HomePage";
 import RecipeDetail from './components/RecipeDetail';
 
@@ -30,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      return <HomePage />;
+     
          <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
