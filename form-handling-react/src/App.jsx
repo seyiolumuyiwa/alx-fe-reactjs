a009fd42f5e3e93a83781e2683f855db1d53cfc7
seyi olumuyiwa <dagaria964@gmail.com>
2025-10-18 04:39:58 +0100

    task 2

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/formikForm";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import PostsComponent from './components/PostsComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,6 +38,17 @@ function App() {
 
       {/* Formik Version */}
       <FormikForm />
+      <PostsComponent />
+
+      const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
     </>
   )
 }
