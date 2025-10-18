46c0dc5bea7f772026d9fd0764e69b51bece7128
seyi olumuyiwa <dagaria964@gmail.com>
2025-10-18 04:58:19 +0100

    wrong commit error fixed

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/formikForm";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import PostsComponent from './components/PostsComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -38,17 +36,6 @@ function App() {
 
       {/* Formik Version */}
       <FormikForm />
-      <PostsComponent />
-
-      const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
-);
     </>
   )
 }
